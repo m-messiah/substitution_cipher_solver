29b090c755e035f9a5ebe56b5a06f4bc615dd3fe
Messiah <m.muzafarov@gmail.com>
2013-05-22 11:02:51 +0600

    russian encrypt

--- encrypt.py
+++ encrypt.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python
-
+# -*- coding: utf-8 -*-
 import random
 import string
 
-abc = "abcdefghijklmnopqrstuvwxyz"
+# LANG = 0 -> EN, 1 -> RUS
+LANG = 1
+
+if LANG:
+    abc = u'абвгдежзиклмнопрстуфхцчшщъыьэюя'
+else:
+    abc = "abcdefghijklmnopqrstuvwxyz"
+
 
 key = list(abc)
 random.shuffle(key)
-key = "".join(key)
+key = ''.join(key)
 
-text = open('text.txt').read().lower()
+if LANG:
+    trantab = dict((ord(a), ord(b)) for a, b in zip(abc, key))
+    text = (open('textR.txt').read()
+                            .decode("string_escape")
+                            .decode("utf-8")
+                            .lower())
+    print text.translate(trantab).encode('utf-8')
 
-trans = string.maketrans(abc, key)
+else:
+    trantab = string.maketrans(abc, key)
+    text = open('text.txt').read().lower()
+    print text.translate(trantab)
 
-print text.translate(trans)
